49f2176adcdc943e385fb39cf4cd1120ac6610d6
David Batranu <david.batranu@eaudeweb.ro>
2024-12-20 19:00:17 +0200

    Attempt to fix tests.

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='pas.plugins.eea',
-    version='1.0a1',
+    name="pas.plugins.eea",
+    version="1.0a1",
     description="Provides user and group enumeration on top of pas.plugins.authomatic",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
@@ -31,40 +33,40 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone CMS',
-    author='David Bătrânu',
-    author_email='david.batranu@eaudeweb.ro',
-    url='https://github.com/collective/pas.plugins.eea',
+    keywords="Python Plone CMS",
+    author="David Bătrânu",
+    author_email="david.batranu@eaudeweb.ro",
+    url="https://github.com/collective/pas.plugins.eea",
     project_urls={
-        'PyPI': 'https://pypi.org/project/pas.plugins.eea/',
-        'Source': 'https://github.com/collective/pas.plugins.eea',
-        'Tracker': 'https://github.com/collective/pas.plugins.eea/issues',
+        "PyPI": "https://pypi.org/project/pas.plugins.eea/",
+        "Source": "https://github.com/collective/pas.plugins.eea",
+        "Tracker": "https://github.com/collective/pas.plugins.eea/issues",
         # 'Documentation': 'https://pas.plugins.eea.readthedocs.io/en/latest/',
     },
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['pas', 'pas.plugins'],
-    package_dir={'': 'src'},
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["pas", "pas.plugins"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.7",
     install_requires=[
-        'setuptools',
+        "setuptools",
         # -*- Extra requirements: -*-
-        'z3c.jbot',
-        'plone.api>=1.8.4',
-        'plone.app.dexterity',
-        'pas.plugins.authomatic',
+        "z3c.jbot",
+        "plone.api>=1.8.4",
+        "plone.app.dexterity",
+        "pas.plugins.authomatic",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
