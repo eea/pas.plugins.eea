67f0bcfb14a1636e8700295ae851c7ceceb5d94b
David Batranu <david.batranu@eaudeweb.ro>
2025-01-22 13:42:09 +0200

    Extend sync.
    * remove users missing from AD
    * update data of existing users
    * also save groups that are group members

--- src/pas/plugins/eea/browser/user_sync.py
+++ src/pas/plugins/eea/browser/user_sync.py
@@ -31,10 +31,7 @@ class UserSyncForm(AutoExtensibleForm, form.EditForm):
             self.status = self.formErrorsMessage
             return
 
-        t0 = datetime.now()
-        count_users, count_groups, done = self.do_sync()
-        seconds = (datetime.now() - t0).total_seconds()
-
+        count_users, count_groups, seconds, done = self.do_sync()
         self.status = f"Synced {count_users} users and {count_groups} groups in {seconds} seconds ({done})"
 
     @button.buttonAndHandler("Cancel")
@@ -43,13 +40,20 @@ class UserSyncForm(AutoExtensibleForm, form.EditForm):
         return self.request.response.redirect(api.portal.get().absolute_url())
 
     def do_sync(self):
+        t0 = datetime.now()
         syncer = SyncEntra()
-        syncer.sync_users()
-        syncer.sync_groups()
-        syncer.sync_group_members()
+        # syncer.sync_all()
+        seconds = (datetime.now() - t0).total_seconds()
+        logger.info(
+            "Synced %s users and %s groups in %s seconds.",
+            syncer.count_users,
+            syncer.count_groups,
+            seconds,
+        )
 
         return (
             syncer.count_users,
             syncer.count_groups,
+            seconds,
             datetime.isoformat(datetime.now()),
         )
--- src/pas/plugins/eea/sync.py
+++ src/pas/plugins/eea/sync.py
@@ -47,6 +47,8 @@ class SyncEntra:
     _provider_name: str = None
     _qm: QueryEntra = None
 
+    _new_users: set = None
+
     count_users: int = 0
     count_groups: int = 0
 
@@ -54,6 +56,7 @@ class SyncEntra:
         self._plugin_eea = get_plugin()
         self._plugin_authomatic = get_authomatic_plugin()
         self._init_query_manager()
+        self._new_users = set()
 
     def _init_query_manager(self):
         settings = authomatic_cfg()
@@ -63,6 +66,7 @@ class SyncEntra:
             cfg["consumer_key"], cfg["consumer_secret"], cfg["domain"]
         )
 
+        self._cfg = cfg
         self._provider_name = provider_name
         self._qm = QueryEntra(query_config)
 
@@ -83,36 +87,84 @@ class SyncEntra:
         ]
         return match[0] if match else None
 
+    def _get_user_email(self, user: ApiUser):
+        user_email = user["mail"] or user["userPrincipalName"]
+
+        if "#EXT#" in user_email:
+            user_email = ""
+
+        return user_email
+
+    def _create_property_sheet(self, plone_uuid, user: ApiUser):
+        pdata = dict(id=plone_uuid)
+        for akey, pkey in self._cfg.get("sync_propertymap", {}).items():
+            if not pdata.get(pkey):
+                pdata[pkey] = user.get(akey, "")
+        pdata["email"] = self._get_user_email(user)
+        sheet = UserPropertySheet(**pdata)
+        return sheet
+
     def remember_user(self, user: ApiUser):
         user_key = (self._provider_name, user["id"])
         plone_uuid = str(uuid.uuid4())
-
-        user_email = user["mail"] or user["userPrincipalName"]
-        if "#EXT#" in user_email:
-            user_email = ""
 
         uis = UserIdentities(plone_uuid)
         uis._identities[self._provider_name] = UserIdentity(
             MockAuthResult(self._provider_name, user)
         )
-        uis._sheet = UserPropertySheet(
-            id=plone_uuid,
-            schema=None,
-            **{"fullname": user["displayName"], "email": user_email},
-        )
+        uis._sheet = self._create_property_sheet(plone_uuid, user)
         self._plugin_authomatic._useridentities_by_userid[plone_uuid] = uis
         self._plugin_authomatic._userid_by_identityinfo[user_key] = plone_uuid
 
         self.count_users += 1
+        self._new_users.add(user["id"])
         logger.info("Added new user: %s (%s)", plone_uuid, user["displayName"])
 
-    def sync_users(self):
+    def add_new_users(self):
         for user in self._qm.get_all_users():
             plone_uuid = self.get_plone_uuid(user["id"])
             if not plone_uuid:
                 self.remember_user(user)
 
+    def remove_missing_users(self):
+        active_remote_uuids = {
+            self.get_plone_uuid(user["id"])
+            for user in self._qm.get_all_users()
+        }
+        local_uuids = {
+            x for x in self._plugin_authomatic._useridentities_by_userid.keys()
+        }
+        to_delete = local_uuids.difference(active_remote_uuids)
+        for plone_uuid in to_delete:
+            service_uuid = self.get_service_uuid(plone_uuid)
+            user_key = (self._provider_name, service_uuid)
+            uis = self._plugin_authomatic._useridentities_by_userid[plone_uuid]
+            user_props = uis._identities[self._provider_name]._sheet
+            user_fullname = (
+                user_props.getProperty("fullname")
+                if user_props
+                else "User has no propertysheet."
+            )
+            del self._plugin_authomatic._useridentities_by_userid[plone_uuid]
+            del self._plugin_authomatic._userid_by_identityinfo[user_key]
+            logger.info("Removed user: %s (%s)", plone_uuid, user_fullname)
+
+    def update_user_data(self):
+        for user in self._qm.get_all_users():
+            if user["id"] in self._new_users:
+                # Skip newly added users (they're already up to date).
+                continue
+            plone_uuid = self.get_plone_uuid(user["id"])
+            if plone_uuid:
+                uis = self._plugin_authomatic._useridentities_by_userid[
+                    plone_uuid
+                ]
+                uis._identities[self._provider_name]._sheet = (
+                    self._create_property_sheet(plone_uuid, user)
+                )
+
     def sync_groups(self):
+        self._plugin_eea._ad_groups.clear()
         for group in self._qm.get_all_groups():
             self._plugin_eea._ad_groups[group["id"]] = group["displayName"]
             self.count_groups += 1
@@ -133,5 +185,14 @@ class SyncEntra:
                     )
                     self.remember_user_group(plone_uuid, group_id)
                 elif member["@odata.type"] == "#microsoft.graph.group":
-                    # TODO: remember groups as group members?
-                    continue
+                    self._plugin_eea._ad_group_members[group_id].add(
+                        member["id"]
+                    )
+                    self.remember_user_group(member["id"], group_id)
+
+    def sync_all(self):
+        self.add_new_users()
+        self.remove_missing_users()
+        self.update_user_data()
+        self.sync_groups()
+        self.sync_group_members()
